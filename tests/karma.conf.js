4d4934ed4c4693c00635be85da9cd69ab0f35ab4
Avnerus <avnerus@gmail.com>
2021-12-17 21:27:08 +0200

    Use ChromeHeadlessNoSandbox

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -4,7 +4,13 @@ process.env.CHROME_BIN = require('puppeteer').executablePath();
 module.exports = function (config) {
   config.set({
     basePath: '../',
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+        ChromeHeadlessNoSandbox: {
+            base: 'ChromeHeadless',
+            flags: ['--no-sandbox']
+        }
+    },
     client: {
       captureConsole: true,
       mocha: { ui: 'tdd' }
